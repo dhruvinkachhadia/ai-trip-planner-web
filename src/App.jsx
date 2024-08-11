c2854058e80a4e749b30f70ee1639b7265864295
Dhruvin Kachhadia <dhruvinkachhadia.dk@gmail.com>
2024-08-11 20:26:37 +0530

    Fix import path for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
-import Hero from './components/ui/custom/hero'
+import HeroComponent from './components/ui/custom/HeroComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       {/*Hero */}
-       <Hero/>
+       {/* Hero */}
+       <HeroComponent/>
     </>
   )
 }
